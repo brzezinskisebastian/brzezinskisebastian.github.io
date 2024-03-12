9add6c7992010f7b4cf6a478ab2c5b9a53d0311c
brzezinskisebastian <152689307+brzezinskisebastian@users.noreply.github.com>
2024-03-12 11:16:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ var lastScrollTop = 0;
 
 // Funkcja do obsługi zdarzenia przewijania strony
 window.addEventListener("scroll", function() {
-    // Pobierz aktualną pozycję pionową przewijania strony względem paska nawigacyjnego
+    // Pobierz aktualną pozycję pionową przewijania strony
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     // Sprawdź, czy użytkownik przewija stronę w dół i czy jest na dół wystarczająco, aby schować pasek
     if (scrollTop > lastScrollTop && scrollTop > topBarHeight) {
-        // Schowaj pasek nawigacyjny
+        // Schowaj pasek nawigacyjny z animacją
+        topBar.style.transition = "top 0.3s";
         topBar.style.top = "-" + topBarHeight + "px";
     } else {
-        // Pokaż pasek nawigacyjny
+        // Pokaż pasek nawigacyjny z animacją
+        topBar.style.transition = "top 0.3s";
         topBar.style.top = "0";
     }
 
