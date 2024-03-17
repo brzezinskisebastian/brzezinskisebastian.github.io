262c07b08e407b562adccd2b91e42ccc573775b3
brzezinskisebastian <152689307+brzezinskisebastian@users.noreply.github.com>
2024-03-17 11:14:04 +0100

    Add files via upload

--- addUpdate.js
+++ addUpdate.js
@@ -1,16 +1,13 @@
 // addUpdate.js
 function addUpdate() {
-    const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
 
     const newUpdate = {
-        title: title,
         content: content
     };
 
     firebase.database().ref('updates').push(newUpdate)
         .then(() => {
-            document.getElementById('title').value = '';
             document.getElementById('content').value = '';
         })
         .catch(error => console.error(error));
