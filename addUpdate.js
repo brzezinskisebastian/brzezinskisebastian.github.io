ad0860c1de821201c6b86f7cf5f95774f8b9c9bb
brzezinskisebastian <152689307+brzezinskisebastian@users.noreply.github.com>
2024-03-17 10:44:51 +0100

    Add files via upload

---
+++ addUpdate.js
@@ -0,0 +1,32 @@
+// addUpdate.js
+// Obsługa dodawania nowej aktualności po submit formularza
+const addUpdateForm = document.getElementById('add-update-form');
+
+addUpdateForm.addEventListener('submit', e => {
+    e.preventDefault(); // Zapobiegamy domyślnej akcji formularza
+
+    const title = addUpdateForm['title'].value;
+    const content = addUpdateForm['content'].value;
+
+    addUpdateToDatabase(title, content);
+
+    addUpdateForm.reset(); // Wyczyść pola formularza po dodaniu aktualności
+});
+
+// Dodawanie nowej aktualności do bazy danych
+function addUpdateToDatabase(title, content) {
+    const updatesRef = firebase.database().ref('updates');
+
+    updatesRef.push({
+        title: title,
+        content: content
+    })
+    .then(() => {
+        console.log('Nowa aktualizacja została dodana!');
+        alert('Nowa aktualizacja została dodana!');
+    })
+    .catch(error => {
+        console.error('Błąd dodawania aktualizacji:', error);
+        alert('Błąd dodawania aktualizacji. Spróbuj ponownie.');
+    });
+}
