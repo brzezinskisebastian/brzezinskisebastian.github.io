e009b7409a293ab905255b65662cf12fec1ea629
brzezinskisebastian <152689307+brzezinskisebastian@users.noreply.github.com>
2024-03-18 22:24:04 +0100

    Add files via upload

---
+++ HTMLPAGE/firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+// Konfiguracja Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyApg246OwWYfdvW2Q7WakOxKCJuno3xVpA",
+    authDomain: "updates-194ca.firebaseapp.com",
+    databaseURL: "https://updates-194ca-default-rtdb.firebaseio.com",
+    projectId: "updates-194ca",
+    storageBucket: "updates-194ca.appspot.com",
+    messagingSenderId: "1003036483034",
+    appId: "1:1003036483034:web:4a7a2df9431817150db4e3"
+  };
+
+// Inicjalizacja Firebase
+firebase.initializeApp(firebaseConfig);
